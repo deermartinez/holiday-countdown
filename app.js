852dd5489237326863315153148d22853934aaff
Destynee Martine <deermartinez8@gmail.com>
2020-12-01 18:39:30 -0700

    alerts/prompts/confirm

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 console.log("Henlo");
 
 
+alert("My name is Destiny...yah?"); //gives a pop up with information to user//
+confirm("Are you at least 18?");  //Asks a yes or no//
+prompt("Are you....?"); //allows user to type//
+
 // this will get the time
 
 
