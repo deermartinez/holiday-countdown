55ec37799998adbaf28b9bd319ea2b78e3f1a86b
Destynee Martine <deermartinez8@gmail.com>
2021-01-03 20:02:07 -0700

    until day

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ console.log("Henlo");
 
 // this will get the time//
 // var used to store data/information that we can use again--diff data types//  
-// var name="Dest";  //string data type//
-// var age=20;         //number-doesnt need quotations//
-// var favFood="HotPot";       //string//
-// var isHungry=true;          //boolean=true/false//
-// var dayOfWeek=["Monday", "Tuesday", "Wednesday", "Thursday"];           //array list of items//
+ var name="Dest";  //string data type//
+ var age=20;         //number-doesnt need quotations//
+var favFood="HotPot";       //string//
+var isHungry=true;          //boolean=true/false//
+var dayOfWeek=["Monday", "Tuesday", "Wednesday", "Thursday"];           //array list of items//
 
 // console.log(name+age+favFood);
 // alert('Welcome' + ' ' + name + '' + age + '' + favFood);
@@ -37,6 +37,49 @@ date.innerHTML = currentTime.toLocaleDateString();
 var daysOfTheWeek = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 var dow = document.getElementById("day");
 dow.innerHTML = daysOfTheWeek[currentTime.getDay()];
+
+
+
+
+//Counting down to
+var countDownDate = new Date("Dec 31, 2021 12:00:00").getTime();
+
+//seconds
+var x = setInterval(function() {
+
+//today date and time
+var now = new Date().getTime();
+  
+// distance between now and the count down date
+var distance = countDownDate - now;
+  
+// days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+// result in the element with id="demo"
+ document.getElementById("demo").innerHTML = days + " days " + hours + " hours "
++ minutes + " minutes " + seconds + " seconds ";
+  
+// Text when countdown complete
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "HAPPY";
+    }
+}, 1000);
+
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+
+
+
+
 
 
 
